de5e66a1ad35463d53517d6cb366a7098242a2f9
ioannisman <82335402+ioannisman@users.noreply.github.com>
2026-03-27 19:04:02 +0100

    [FEATURE] github actions with build and tests (#4)
    * github actions with gradle setup and smoke test
    
    * failing test
    
    * Revert "failing test"
    
    This reverts commit 6813a50bf06b363168c20d13b4769de16cb86634.

---
+++ src/test/java/rvd/SmokeTest.java
@@ -0,0 +1,13 @@
+package rvd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SmokeTest {
+
+    @Test
+    void sanity() {
+        assertTrue(true);
+    }
+}
